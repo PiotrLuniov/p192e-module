5efc9018ffe1d20e9def6e33c464930854bfe6a7
Hanna Bledai <student@EPBYMINW5249.minsk.epam.com>
2019-08-08 18:26:53 +0300

    add src

--- src/K8s.groovy
+++ src/K8s.groovy
@@ -1,10 +1,10 @@
 def createSecret (def namesecret = 'regcred', def url = 'nexus-ci.playpit.by:6566', def username = 'admin', def password = 'admin123'){
-		sh '''
-		if [ ! $(kubectl get secret -n hbledai | grep -q \${namesecret} && echo $?) ]
+		sh """
+		if [ ! \$(kubectl get secret | grep -q ${namesecret} && echo \$?) ]
     then
-		 "kubectl create secret docker-registry \${namesecret} --docker-server=\${url} --docker-username=\${username} --docker-password=\${password}"
+		 "kubectl create secret docker-registry ${namesecret} --docker-server=${url} --docker-username=${username} --docker-password=\${password}"
 	fi
-	'''
+	"""
 	}
 
 return this 

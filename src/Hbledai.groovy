1f46dfda897d51961d90a386c63c8add831949be
Hanna Bledai <student@EPBYMINW5249.minsk.epam.com>
2019-08-08 17:12:39 +0300

    add src

--- src/Hbledai.groovy
+++ src/Hbledai.groovy
@@ -1,22 +1,15 @@
-	/*class K8s{
+package  _	
+	class Hbledai{
 	def steps
 	def namesecret = ''
 	def url_docker_repo = ''
 	def username = ''
 	def password = ''
-	K8s(steps) {this.steps = steps}
+	Hbledai(steps) {this.steps = steps}
 	
 	def createSecret (){
 		steps.sh "kubectl create secret docker-registry ${name} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
 	}
 }
 
-return this*/
-package _
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-}
+return this

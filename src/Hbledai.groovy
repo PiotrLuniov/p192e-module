565770509b56cd7f9f594dad47226e881e8de437
Hanna Bledai <student@EPBYMINW5249.minsk.epam.com>
2019-08-08 17:04:45 +0300

    add src

--- src/Hbledai.groovy
+++ src/Hbledai.groovy
@@ -1,11 +1,15 @@
 	package _
+	class K8s{
+	def steps
 	def namesecret = ''
 	def url_docker_repo = ''
 	def username = ''
 	def password = ''
-
-	def create_secret (){
-		sh "kubectl create secret docker-registry ${name} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
+	K8s(steps) {this.steps = steps}
+	
+	def createSecret (){
+		steps.sh "kubectl create secret docker-registry ${name} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
 	}
+}
 
 return this

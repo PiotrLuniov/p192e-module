41e7b8aeed109b65abf0ee5d1efe9d3ebd4c650b
Hanna Bledai <student@EPBYMINW5249.minsk.epam.com>
2019-08-08 18:09:43 +0300

    add src

--- vars/configureSecret.groovy
+++ vars/configureSecret.groovy
@@ -1,8 +1,4 @@
-def call(String namesecret, String url , String username , String password  ) {
-		namesecret = 'regcred'
-		url = 'nexus-ci.playpit.by:6566'
-		username = 'admin'
-		password = 'admin123'
-		sh "kubectl create secret docker-registry ${name} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
+def call(def namesecret = 'regcred', def url = 'nexus-ci.playpit.by:6566', def username = 'admin', def password = 'admin123' ) {
+		sh "kubectl create secret docker-registry ${namesecret} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
 
 }

8edf8ba1acf0e979ddc44b6610657339e246f259
Hanna Bledai <student@EPBYMINW5249.minsk.epam.com>
2019-08-08 15:53:10 +0300

    add vars

---
+++ vars/hbledai.groovy
@@ -0,0 +1,10 @@
+class Kubectl {
+	def namesecret = ''
+	def url_docker_repo = ''
+	def username = ''
+	def password = ''
+
+	def create_secret (name, url, username, password){
+		sh "kubectl create secret docker-registry ${name} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
+	}
+}

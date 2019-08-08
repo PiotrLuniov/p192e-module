751146ad64c79fb4d7a7281a232b26b344e899eb
Hanna Bledai <student@EPBYMINW5249.minsk.epam.com>
2019-08-08 16:37:42 +0300

    add src

--- src/hbledai.groovy
+++ src/hbledai.groovy
@@ -1,9 +1,10 @@
+	package _
 	def namesecret = ''
 	def url_docker_repo = ''
 	def username = ''
 	def password = ''
 
-	def create_secret (name, url, username, password){
+	def create_secret (){
 		sh "kubectl create secret docker-registry ${name} --docker-server=${url} --docker-username=${username} --docker-password=${password}"
 	}
 

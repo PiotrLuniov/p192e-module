321afd9b21d6de223b686f28f9110e65885d2e1d
AliaksandrShamchonak <Aliaksandr_Shamchonak@epam.com>
2019-08-08 22:53:09 +0300

    Task11

---
+++ cert/cert.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Installing Helm
+#https://www.digitalocean.com/community/tutorials/how-to-install-software-on-kubernetes-clusters-with-the-helm-package-manager
+cd /tmp
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
+chmod u+x install-helm.sh
+./install-helm.sh
+#Helm: bind the tiller serviceaccount to the cluster-admin role
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+
+
+#Installing and Configuring Cert-Manager
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-an-nginx-ingress-with-cert-manager-on-digitalocean-kubernetes
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
+kubectl label namespace kube-system certmanager.k8s.io/disable-validation="true"
+helm repo add jetstack https://charts.jetstack.io
+helm install --name cert-manager --namespace kube-system jetstack/cert-manager --version v0.8.0
+kubectl apply -f cert/staging_issuer.yaml

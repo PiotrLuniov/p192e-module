7326619f20b4d4fecb033d4d9b70571c05420c5c
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-08-09 20:46:27 +0300

    Update megaPush.groovy

--- vars/megaPush.groovy
+++ vars/megaPush.groovy
@@ -1,24 +1,18 @@
 def call(String artifactType, String studentName){
-
-  if (artifactType == "maven"){
-				nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'MNT-pipeline-training', \
-					packages: [[$class: 'MavenPackage', \
-						mavenAssetList: [[classifier: '', extension: '', \
-							filePath: "pipeline-${studentName}-\${BUILD_NUMBER}.tar.gz"]], \
-						mavenCoordinate: [artifactId: "${studentName}", groupId: 'pipeline', \
-							packaging: 'tar.gz', version: '${BUILD_NUMBER}'] \
-					]]
-    }
-  if (artifactType == "docker"){
-              withDockerRegistry(credentialsId: 'nexus', toolName: 'dockerTool', \
-						   url: 'http://nexus-ci.playpit.by:6566') {
-
-					sh "ls -la"
-					// sh "docker build -t helloworld-${studentName}:${BUILD_NUMBER} -f Dockerfile ."
-					sh "docker build -t nexus-ci.playpit.by:6566/helloworld-${studentName}:${BUILD_NUMBER} -f Dockerfile ."
-					//sh "docker images"
-					sh "docker push nexus-ci.playpit.by:6566/helloworld-${studentName}:${BUILD_NUMBER}"
-					sh "docker images"
-				}
-  }
+	if (artifactType == "maven"){
+		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'MNT-pipeline-training', \
+			packages: [[$class: 'MavenPackage', \
+				mavenAssetList: [[classifier: '', extension: '', \
+					filePath: "pipeline-${studentName}-\${BUILD_NUMBER}.tar.gz"]], \
+				mavenCoordinate: [artifactId: "${studentName}", groupId: 'pipeline', \
+					packaging: 'tar.gz', version: '${BUILD_NUMBER}'] \
+			]]
+	}
+	if (artifactType == "docker"){
+		withDockerRegistry(credentialsId: 'nexus', toolName: 'dockerTool', \
+					url: 'http://nexus-ci.playpit.by:6566') {
+		sh "docker build -t nexus-ci.playpit.by:6566/helloworld-${studentName}:${BUILD_NUMBER} -f Dockerfile ."
+		sh "docker push nexus-ci.playpit.by:6566/helloworld-${studentName}:${BUILD_NUMBER}"
+		}
+	}
 }

8e03c9e5046b94eae48bcc3aad114dcb2477e298
Antoś Bućko <anton_butsko@epam.com>
2019-08-09 17:59:12 +0300

    Set default value

--- src/abutsko.groovy
+++ src/abutsko.groovy
@@ -1,4 +1,4 @@
-def pushDockerImage(String nameImage, String nameRepo, String credentialId, String Dockerfile) {
+def pushDockerImage(String nameImage, String nameRepo, String credentialId, String Dockerfile = 'Dockerfile') {
     docker.withRegistry("https://${nameRepo}", "${credentialId}") {
         def appImage = docker.build("${nameRepo}/${nameImage}", "-f ${Dockerfile} .")
         appImage.push()

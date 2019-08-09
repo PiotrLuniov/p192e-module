6fe605545de3ae6f0e117d0b941713c9f8679727
Antoś Bućko <anton_butsko@epam.com>
2019-08-09 18:03:58 +0300

    Correct mistakes

--- src/abutsko.groovy
+++ src/abutsko.groovy
@@ -1,5 +1,5 @@
 def pushDockerImage(String nameImage, String nameRepo, String credentialId, String Dockerfile = 'Dockerfile') {
-    docker.withRegistry("https://${nameRepo}", "${credentialId}") {
+    docker.withRegistry("http://${nameRepo}", "${credentialId}") {
         def appImage = docker.build("${nameRepo}/${nameImage}", "-f ${Dockerfile} .")
         appImage.push()
     }

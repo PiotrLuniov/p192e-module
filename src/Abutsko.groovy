cffbd94bb54e4be6898e7b824659363557821d4d
Paladin-Dranser <anton.butsko@gmail.com>
2019-08-11 15:19:06 +0300

    testing

--- src/Abutsko.groovy
+++ src/Abutsko.groovy
@@ -1,8 +1,8 @@
-def pushDockerImage(String nameImage, String nameRepo, String credentialId, String Dockerfile = 'Dockerfile') {
-    docker.withRegistry("http://${nameRepo}", "${credentialId}") {
-        def appImage = docker.build("${nameRepo}/${nameImage}", "-f ${Dockerfile} .")
-        appImage.push()
-    }
-}
+//def pushDockerImage(String nameImage, String nameRepo, String credentialId, String Dockerfile = 'Dockerfile') {
+//    docker.withRegistry("http://${nameRepo}", "${credentialId}") {
+//        def appImage = docker.build("${nameRepo}/${nameImage}", "-f ${Dockerfile} .")
+//        appImage.push()
+//    }
+//}
 
 return this
